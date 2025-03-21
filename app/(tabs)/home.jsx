45f23a159b8dd96911a4b99686cb23f60bcd3555
riotzyoldic <waghelasoham@gmail.com>
2025-03-21 23:36:51 +0530

    TabBar and home page header

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,26 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Header from "../../Components/Home/Header";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        padding: 20,
+        // marginTop: ,
+      }}
+    >
+      {/* Header */}
+
+      <Header />
+
+      {/* Slider */}
+
+      {/* Category */}
+
+      {/* List of pets */}
+
+      {/* Add new pet option */}
     </View>
   );
 }
