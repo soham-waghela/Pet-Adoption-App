517a436a266b6a7fc0c569e08ec7927843da3552
riotzyoldic <waghelasoham@gmail.com>
2025-03-25 00:21:41 +0530

    CategoryList

--- Components/Home/Category.jsx
+++ Components/Home/Category.jsx
@@ -1,10 +1,75 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  Dimensions,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/FirebaseConfig";
+import Colors from "./../../constants/Colors";
 
 export default function Category() {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    GetCategories();
+  }, []);
+
+  const GetCategories = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "Category"));
+      const categories = snapshot.docs.map((doc) => doc.data()); // Convert Firestore snapshot to array
+      setCategoryList(categories); // Update state with fetched data
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Category</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Category</Text>
+      <FlatList
+        data={categoryList}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={styles.imageContainer}>
+            <Image
+              source={{ uri: item?.imageurl }}
+              style={styles.categoryImage}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+  },
+  title: {
+    fontFamily: "outfit-medium",
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  imageContainer: {
+    marginHorizontal: 10,
+    backgroundColor: Colors.LIGHT_PRIMARY,
+    padding: 15,
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: Colors.PRIMARY,
+  },
+  categoryImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+});
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,4 +2,6 @@ export default {
   PRIMARY: "#E8B20E",
   GRAY: "#8F8E8D",
   WHITE: "#FFF",
+  SECONDARY: "#4faaff",
+  LIGHT_PRIMARY: "#fff1c9",
 };
