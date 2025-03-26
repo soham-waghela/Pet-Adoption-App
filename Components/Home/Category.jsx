a45fc9143811f631836aa96a4da0ad6a2f2e461e
riotzyoldic <waghelasoham@gmail.com>
2025-03-26 23:48:28 +0530

    Category changes

--- Components/Home/Category.jsx
+++ Components/Home/Category.jsx
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Text,
   Dimensions,
+  TouchableOpacity,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
@@ -13,6 +14,7 @@ import Colors from "./../../constants/Colors";
 
 export default function Category() {
   const [categoryList, setCategoryList] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("Dog");
 
   useEffect(() => {
     GetCategories();
@@ -37,12 +39,24 @@ export default function Category() {
         horizontal
         showsHorizontalScrollIndicator={false}
         renderItem={({ item }) => (
-          <View style={styles.imageContainer}>
-            <Image
-              source={{ uri: item?.imageurl }}
-              style={styles.categoryImage}
-            />
-          </View>
+          <TouchableOpacity
+            onPress={() => setSelectedCategory(item.name)}
+            style={styles.itemContainer}
+          >
+            <View
+              style={[
+                styles.imageContainer,
+                selectedCategory == item.name &&
+                  styles.selectedCategoryContainer,
+              ]}
+            >
+              <Image
+                source={{ uri: item?.imageurl }}
+                style={styles.categoryImage}
+              />
+            </View>
+            <Text style={styles.categoryText}>{item?.name ?? "No Name"}</Text>
+          </TouchableOpacity>
         )}
       />
     </View>
@@ -50,16 +64,11 @@ export default function Category() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 10,
-  },
-  title: {
-    fontFamily: "outfit-medium",
-    fontSize: 20,
-    marginBottom: 10,
+  itemContainer: {
+    alignItems: "center",
+    marginHorizontal: 9,
   },
   imageContainer: {
-    marginHorizontal: 10,
     backgroundColor: Colors.LIGHT_PRIMARY,
     padding: 15,
     alignItems: "center",
@@ -67,9 +76,24 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     borderColor: Colors.PRIMARY,
   },
+  title: {
+    fontFamily: "outfit-medium",
+    fontSize: 20,
+    marginBottom: 10,
+  },
   categoryImage: {
     width: 40,
     height: 40,
     borderRadius: 10,
+  },
+  categoryText: {
+    marginTop: 5,
+    fontSize: 14,
+    fontWeight: "bold",
+    color: Colors.GRAY,
+  },
+  selectedCategoryContainer: {
+    backgroundColor: Colors.SECONDARY,
+    borderColor: Colors.SECONDARY,
   },
 });
